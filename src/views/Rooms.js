bf6df79652a03877362daf6648f43226df44f857
goldipl <mgodlewski92@gmail.com>
2022-03-11 14:42:34 +0100

    change ul to div

--- src/views/Rooms.js
+++ src/views/Rooms.js
@@ -11,14 +11,14 @@ export function Rooms() {
     fetch('https://my-json-server.typicode.com/goldipl/SPA_shop_application/rooms')
         .then(response => response.json())
         .then(rooms => {
-            const ul = document.createElement('ul');
+            const div = document.createElement('div');
 
             const lis = rooms.map(room => Room(room));
 
-            ul.append(...lis);
+            div.append(...lis);
 
             section.querySelector('#loading').remove();
-            section.append(ul);
+            section.append(div);
         });
 
     return section;
