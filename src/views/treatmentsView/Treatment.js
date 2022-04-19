9099c3a0dcbbd68a68af0d1a3978be6f7e9c337b
goldipl <mgodlewski92@gmail.com>
2022-04-19 18:36:47 +0200

    add images to treatments

--- src/views/treatmentsView/Treatment.js
+++ src/views/treatmentsView/Treatment.js
@@ -2,11 +2,13 @@ import { AddToCartButton } from '../../common/AddToCartButton';
 import "./treatment.scss"
 
 export function Treatment(treatment) {
-    const { name, area, time, price } = treatment;
+    const { name, area, time, price, img } = treatment;
     const article = document.createElement('article');
+    article.className = "treatmentContainer";
 
     article.innerHTML = `
     <h4>${name}</h4>
+    <img width="200px" height="auto" src=${img}></img>
     <p>Area: ${area}</p>
     <p>Time: ${time} minutes</p>
     <footer>
