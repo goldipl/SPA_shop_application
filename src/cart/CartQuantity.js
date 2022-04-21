122c545eb2c0e45a5f9075ac10b1d16641ba0be8
goldipl <mgodlewski92@gmail.com>
2022-04-21 17:23:05 +0200

    fix cartQuantity feature after add a new nav item

--- src/cart/CartQuantity.js
+++ src/cart/CartQuantity.js
@@ -1,5 +1,5 @@
 export function CartQuantity() {
-    let cartButton = document.querySelector('nav button:nth-child(7)');
+    let cartButton = document.querySelector('nav button:nth-child(8)');
 
     cartButton.addEventListener("mouseenter", (e) => {
     const cart = localStorage.getItem('spaCart');
