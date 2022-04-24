d0697b8082790519e35b1fc5fc3c950d5915305f
goldipl <mgodlewski92@gmail.com>
2022-04-24 15:22:04 +0200

    update cartQuantity script

--- src/cart/CartQuantity.js
+++ src/cart/CartQuantity.js
@@ -1,9 +1,9 @@
 export function CartQuantity() {
-    let cartButton = document.querySelector('nav button:nth-child(8)');
+    let cartButton = document.querySelector('nav button:nth-child(9)');
 
     cartButton.addEventListener("mouseenter", (e) => {
     const cart = localStorage.getItem('spaCart');
-    cartQuantity = JSON.parse(cart).length;
+    const cartQuantity = JSON.parse(cart).length;
     alert(`Cart Quantity = ${cartQuantity}`);
     });
 };
