9dea4e941ab16a73455ebf1bed273ed1224db34d
goldipl <mgodlewski92@gmail.com>
2022-05-16 21:19:41 +0200

    styling backButton

--- src/common/BackButton.js
+++ src/common/BackButton.js
@@ -2,7 +2,9 @@ export function BackButton(Component) {
    const button = document.createElement('button');
     button.setAttribute('type', 'button');
     button.classList.add('btn', 'btn-primary');
-    button.style.margin = '1em 0';
+    button.style.margin = '1em auto';
+    button.style.display = 'block';
+    button.style.width = '200px';
     button.innerText = 'Back';
 
     const navigateEvent = new CustomEvent('navigate', {
