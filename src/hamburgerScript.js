bf34bbfe86f8273a70be1687197f33f877a44081
goldipl <mgodlewski92@gmail.com>
2022-05-22 15:03:11 +0200

    add chaning ☰ to ✖ using hamburger js

--- src/hamburgerScript.js
+++ src/hamburgerScript.js
@@ -5,8 +5,10 @@ hamburger.addEventListener('click', () => {
     menuOn.forEach(button => {
         if (button.classList.contains("hamburgerOn")) {
             button.classList.remove("hamburgerOn");
+            hamburger.innerText = "☰";
         } else {
             button.classList.add("hamburgerOn");
+            hamburger.innerText = "✖";
         }         
     });
 });
